d764df109e0dc64f4e6e8d5f76b63c19bdf6c3bf
cloudmato <cloudmato00@gmail.com>
2025-01-03 11:49:16 +0900

    feat(객체 도입)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,8 @@ class App {
         String[] contentList = new String[3];
         String[] authorList = new String[3];
 
+        WiseSaying wiseSaying = new WiseSaying();
+
         while (true) {
 
             System.out.print("명령: ");
@@ -30,23 +32,35 @@ class App {
             } else if (command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                contentList[lastNo] = sc.nextLine();  // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                String content = sc.nextLine();  // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
-                authorList[lastNo] = sc.nextLine();
+                String author = sc.nextLine();
 
                 //System.out.println(lastNo + " 명언이 등록되었습니다.");
                 //lastNo++;
 
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastNo));
+                wiseSaying.id = ++lastNo;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo));
             } else if (command.equals("목록")) {
 
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for (int i = 2; i >= 0; i--) {
-                    System.out.println("%d / %s / %s".formatted(i, authorList[i], contentList[i]));
-                }
+                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+
+                //for (int i = 2; i >= 0; i--) {
+                //    System.out.println("%d / %s / %s".formatted(i, authorList[i], contentList[i]));
+                //}
             }
         }
     }
+}
+
+class WiseSaying {
+    int id;
+    String content;
+    String author;
 }
